f665fa8acaca39606c6546045e6d64b3837d6049
VLADISLAV <SIZONOV.95@MAIL.RU>
2021-11-07 18:14:23 +0300

    Initial Commit

---
+++ TestMapKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestMapKit
+//
+//  Created by Владислав Сизонов on 07.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
