0faa7b7119ef8ae7f75692181945ec8b6fe87a1d
VLADISLAV <SIZONOV.95@MAIL.RU>
2021-11-07 20:22:56 +0300

    layout

---
+++ Alert.swift
@@ -0,0 +1,8 @@
+//
+//  Alert.swift
+//  TestMapKit
+//
+//  Created by Владислав Сизонов on 07.11.2021.
+//
+
+import Foundation
--- TestMapKit/ViewController.swift
+++ TestMapKit/ViewController.swift
@@ -6,14 +6,102 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+    let mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        return mapView
+    }()
+    
+    let addAdressButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Add adress", for: .normal)
+        button.backgroundColor = .gray
+        button.layer.cornerRadius = 8
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let routeButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Route", for: .normal)
+        button.backgroundColor = .gray
+        button.layer.cornerRadius = 8
+        button.isHidden = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let resetButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Reset", for: .normal)
+        button.backgroundColor = .gray
+        button.layer.cornerRadius = 8
+        button.isHidden = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupConstraints()
+        
+        addAdressButton.addTarget(self, action: #selector(addAdressButtonTapped), for: .touchUpInside)
+        routeButton.addTarget(self, action: #selector(routeButtonTapped), for: .touchUpInside)
+        resetButton.addTarget(self, action: #selector(resetButtonTapped), for: .touchUpInside)
     }
-
-
+    
+    @objc func addAdressButtonTapped() {
+        
+    }
+    
+    @objc func routeButtonTapped() {
+        
+    }
+    
+    @objc func resetButtonTapped() {
+        
+    }
 }
 
+extension ViewController {
+    
+    func setupConstraints() {
+    
+        view.addSubview(mapView)
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
+        ])
+        
+        mapView.addSubview(addAdressButton)
+        NSLayoutConstraint.activate([
+            addAdressButton.topAnchor.constraint(equalTo: mapView.topAnchor, constant: 50),
+            addAdressButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -20),
+            addAdressButton.heightAnchor.constraint(equalToConstant: 36),
+            addAdressButton.widthAnchor.constraint(equalToConstant: 100)
+        ])
+        
+        mapView.addSubview(routeButton)
+        NSLayoutConstraint.activate([
+            routeButton.leadingAnchor.constraint(equalTo: mapView.leadingAnchor, constant: 20),
+            routeButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor , constant: -30),
+            routeButton.heightAnchor.constraint(equalToConstant: 36),
+            routeButton.widthAnchor.constraint(equalToConstant: 100)
+        ])
+        
+        mapView.addSubview(resetButton)
+        NSLayoutConstraint.activate([
+            resetButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -20),
+            resetButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor , constant: -30),
+            resetButton.heightAnchor.constraint(equalToConstant: 36),
+            resetButton.widthAnchor.constraint(equalToConstant: 100)
+        ])
+    }
+}
